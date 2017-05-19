c3898272bc5213e6680436136387d28050466a11
Adam Perlstrom <adam.perlstrom@firstdirect.com>
2017-05-19 15:25:18 +0100

    refactor comps to use arrow functions

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -1,16 +1,26 @@
 var React = require('react'),
     Nav = require('Nav');
 
-var Main = React.createClass({
-    render: function(){
-        return(
-            <div>
-                <Nav />
-                <h2>Main Comp</h2>
-                {this.props.children}
-            </div>
-        );
-    }
-});
+// var Main = React.createClass({
+//     render: function(){
+//         return(
+//             <div>
+//                 <Nav />
+//                 <h2>Main Comp</h2>
+//                 {this.props.children}
+//             </div>
+//         );
+//     }
+// });
+
+var Main = (props) => {
+    return(
+        <div>
+            <Nav />
+            <h2>Main Comp</h2>
+            {props.children}
+        </div>
+    );
+}
 
 module.exports = Main;
--- app/components/weather/weatherMessage.jsx
+++ app/components/weather/weatherMessage.jsx
@@ -1,13 +1,21 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-    render: function(){
-        var {temp, location} = this.props;
+// var WeatherMessage = React.createClass({
+//     render: function(){
+//         var {temp, location} = this.props;
+//
+//         return(
+//             <p>It is {temp}, in {location}</p>
+//         );
+//     }
+// });
 
-        return(
-            <p>It is {temp}, in {location}</p>
-        );
-    }
-});
+var WeatherMessage = (props) => {
+    var {temp, location} = props;
+
+    return(
+        <p>It is {temp}, in {location}</p>
+    );
+}
 
 module.exports = WeatherMessage;
