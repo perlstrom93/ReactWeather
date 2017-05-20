7491f63577a9b0f13e020cae168dac04c2d01e6f
Adam Perlstrom <Adam Perlstrom>
2017-05-20 15:56:03 +0100

    fix more shit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 const PORT=process.env.PORT || 8080;
 
 app.use(function(req, res, next){
-    if(req.headers['x-forward-proto'] === 'https'){
+    if(req.headers['x-forwarded-proto'] === 'http'){
         next();
     } else {
         res.redirect('http://' + req.hostname + req.url);
