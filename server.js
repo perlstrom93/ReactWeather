ebb54126d361bdf106042e082fcd04d4b9ac27ec
Adam Perlstrom <Adam Perlstrom>
2017-05-20 15:32:31 +0100

    prep repo for heroku

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require('express');
 
-//Create our app
-
+// Create our app
 var app = express();
-const PORT = process.env.PORT || 8080;
+const PORT=process.env.PORT || 8080;
 
-app.use(function(req, res, next){
-    if (req.headers['x-forwarded-prot'] == 'htpp') {
+app.user(function(req, res, next){
+    if(req.headers['x-forward-proto'] === 'http'){
         next();
     } else {
         res.redirect('http://' + req.hostname + req.url);
@@ -15,6 +14,6 @@ app.use(function(req, res, next){
 
 app.use(express.static('public'));
 
-app.listen(PORT, function(){
-    console.log('Express server is up on port ' + PORT);
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
